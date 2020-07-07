de20bbf6e16724961e9d94e4b125adf742ffb6b2
du <982830737@qq.com>
2020-07-07 23:33:56 +0800

    update config,py

--- config/config.py
+++ config/config.py
@@ -13,3 +13,9 @@ config.train.dataset = '/home/du/Desktop/work_project/TBox-server-dh/classificat
 config.train.img_size = 224
 config.train.batch_size = 64
 config.train.base_lr = 0.0001
+config.train.epoches = 30
+config.train.mean = [0.485, 0.456, 0.406]
+config.train.std = [0.229, 0.224, 0.225]
+# train --warmup
+config.train.warmup_epoches = 1
+config.train.warmup_init_lr = 1e-7
