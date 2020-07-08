d74897d34c60655543b8d9d34bebbe902020288c
du <982830737@qq.com>
2020-07-09 00:50:29 +0800

    update config parameter

--- configs/config.py
+++ configs/config.py
@@ -17,6 +17,9 @@ config.train.mean = [0.485, 0.456, 0.406]
 config.train.std = [0.229, 0.224, 0.225]
 config.train.checkpoint = f'logs/{config.model.name}'
 
+# train -- optimizer
+config.train.optimizer = CN()
+config.train.optimizer.method = 'adam'
 
 # train -- scheduler
 config.train.scheduler = CN()
@@ -37,9 +40,16 @@ config.train.dataloader.pin_memory = True  # False when memory not enough
 config.train.dataloader.non_blocking = True  # False when memory not enough
 config.train.dataloader.collate_fn = True
 
-# train -- optimizer
-config.train.optimizer = CN()
-config.train.optimizer.method = 'adam'
+# valid
+config.val = CN()
+config.val.batch_size = 64
+
+# valid --dataloader
+config.val.dataloader = CN()
+config.val.dataloader.work_nums = 4
+config.val.dataloader.drop_last = False
+config.train.dataloader.pin_memory = False  # False when memory not enough
+
 
 # tensorboard
 config.tensorboard = CN()
