f9e72bc7ccad0c37d2ba5cc8df87e2110f948b38
du <982830737@qq.com>
2020-07-15 00:24:22 +0800

    update code

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 import torch
 import os
-# model = torch.load('/home/du/Desktop/my_project/pytorch_classification/weights/fold0_lowest_loss.pth')
+
+torch.distributed.init_process_group(backend='nccl',init_method='env://')# model = torch.load('/home/du/Desktop/my_project/pytorch_classification/weights/fold0_lowest_loss.pth')
 # model = torch.load('/home/du/Desktop/my_project/pytorch_classification/logs/2020-07-14 11:11:21/12.pth')
 # print(model)
-print(int(os.environ['SLURM_LOCALID']))
+print(torch.distributed.get_rank())
+# print(int(os.environ['SLURM_LOCALID']))
+
